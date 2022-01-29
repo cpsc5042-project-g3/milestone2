c60eb512ff0943bc1a5bd81ce8e1b4503e39c43f
StoneTrees737797 <80914965+StoneTrees737797@users.noreply.github.com>
2022-01-28 18:52:53 -0800

    Initial commit from local copy.

---
+++ src/Server/cmake-build-debug/CMakeFiles/Server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Server.dir/server2.cpp.o"
+  "Server.exe"
+  "Server.pdb"
+  "libServer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
